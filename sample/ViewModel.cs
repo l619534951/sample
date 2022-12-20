36be628eb3a83be169f0d8b4027d735328a44b63
l619534951 <619534951@qq.com>
2022-12-20 13:23:08 +0800

    first commit

---
+++ sample/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    [ObservableObject]
+    internal partial class ViewModel
+    {
+        [ObservableProperty]
+        private int id;
+
+        partial void OnIdChanged(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ObservableProperty]
+        private string name;
+
+        partial void OnNameChanged(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
